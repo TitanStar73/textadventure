87fe2399435316e0985b6c0cc6e5477ae7d2f4c4
TitanStar73 <153201417+TitanStar73@users.noreply.github.com>
2024-08-22 23:46:38 +0530

    Update slot_machine_test.py

--- slot_machine_test.py
+++ slot_machine_test.py
@@ -33,18 +33,42 @@ SLOT_1_ROLLS = [SLOT_ASSETS[i] for i in range(0,7)]
 SLOT_2_ROLLS = [SLOT_ASSETS[i] for i in range(0,11)]
 SLOT_3_ROLLS = [SLOT_ASSETS[i] for i in range(0,13)]
 SLOT_WINNING = SLOT_ASSETS[0]
-SLOT_REFRESH = f"\033[{len(SLOT_1_ROLLS[0].split('\n')) + 1}A" #Move cursor back up to create animation
+SLOT_REFRESH = f"\033[{len(SLOT_1_ROLLS[0].split('\n'))+1}A" #Move cursor back up to create animation
 
 SLOT_FPS = 30
 SLOT_SLEEP = 1/SLOT_FPS
-SLOT_ANIMATION_TIME = (0.5, 6, 0.2) #(time for each slot, total slots per slot, transition time)
+SLOT_ANIMATION_TIME = (1, 6, 0.35) #(time for each slot to be displayed, total slots per slot, transition time)
 
 def special_print(texts):
-    for row_no in texts[0].split('\n'):
+    for row_no in range(0,len(texts[0].split('\n'))):
         for text in texts:
             print(text.split('\n')[row_no], end = ' ')
         print("")
-    print("")
+
+def get_spliced_asset(a1,a2,ratio): #ratio to ratio + 1 shown, e.g. 0.5 to 1.5
+    total = a1 + "\n" + a2
+    total = total.split("\n")
+
+    final = ""
+    start_line = int(len(a1.split('\n')) * ratio)
+
+    for line_no in range(start_line, start_line + len(a1.split('\n'))):
+        final += total[line_no] + "\n"
+
+    return final
+
+def create_roll(a1,a2, previous = []):
+    frames_for_roll = int(SLOT_FPS*SLOT_ANIMATION_TIME[2])
+    for i in range(0,frames_for_roll-1):
+        previous.append(get_spliced_asset(a1,a2,i/frames_for_roll))
+        special_print(previous)
+        previous.pop()
+        sleep(SLOT_SLEEP)
+        print(SLOT_REFRESH, flush=True)
+    
+    special_print(get_spliced_asset(a1,a2,1))
+    sleep(SLOT_SLEEP)
+    
 
 def play_slot_machine():
     print("Slot machine!")
@@ -55,21 +79,43 @@ def play_slot_machine():
         except:
             print("Please enter a whole number.")
 
-    s1 = randchoice(SLOT_1_ROLLS)
+    s1 = SLOT_ASSETS[0] #randchoice(SLOT_1_ROLLS)
     s2 = randchoice(SLOT_2_ROLLS)
     s3 = randchoice(SLOT_3_ROLLS)
-
-    for i in range(0, SLOT_ANIMATION_TIME[1]):
-        print(randchoice(SLOT_1_ROLLS))
-        sleep(SLOT_ANIMATION_TIME[0])
-        print(SLOT_REFRESH, flush=True)
     
-    print(SLOT_1_ROLLS[0])
+    a2 = randchoice(SLOT_1_ROLLS)
+    for i in range(0, SLOT_ANIMATION_TIME[1]):
+        a1 = a2
+        a2 = randchoice(SLOT_2_ROLLS)
+        create_roll(a1,a2, previous=[s2])
+        print(SLOT_REFRESH, flush=True)
 
+    create_roll(a2,s1, previous=[s2])
+    sleep(SLOT_ANIMATION_TIME[0])
+    
     if s1 == s2 == s3 == SLOT_WINNING:
         print(f"Congratulations! You won {bet*1000} gold!")
         return bet*1000
 
     return -bet
 
+
+print("s")
+print("s")
+print("s")
+print("s")
+print("s")
+
 play_slot_machine()
+
+
+
+
+
+
+print("s")
+print("s")
+print("s")
+print("s")
+print("s")
+print("s")
